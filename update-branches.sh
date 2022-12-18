241d1bef23c59bf57e04b05e47e9d8cc55c397f9
Rainer Hahnekamp <rainer.hahnekamp@gmail.com>
2022-12-18 10:24:44 +0100

    add branch updater

---
+++ update-branches.sh
@@ -0,0 +1,25 @@
+set -e
+
+git checkout main
+
+branches=(main unit-1 unit-2 unit-3)
+previous=
+current=
+
+for branch in ${branches[*]}; do
+  previous=$current
+  current=$next
+  next=$branch
+
+  if [ ! $current = "" ]
+  then
+    git checkout $current
+    git tag -d `git tag | grep -E '.'`
+    git merge $previous -m merge
+  fi;
+done
+
+git checkout $branch
+git merge $current $branch -m merge
+
+git checkout master
